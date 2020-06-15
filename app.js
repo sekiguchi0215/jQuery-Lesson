181f3862b46e0510ed43c2ee7fb3b19f58d1b9dd
関口大介 <sekiguchidaisuke@sekiguchidaisukenoMacBook-Pro.local>
2020-06-15 22:05:57 +0900

    [Start]js-6

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 $(function(){
-	$('.box1').slideDown(2000, function(){
-		$('.box1').css({
-			'background-color': '#0000ff',
-			'width': '200px',
-			'height': '100px'
-		}).slideUp(200);
+	$('.box1').mouseover(function(){
+		$('.box1').css({'background-color': '#0000ff'});
 	});
 });
